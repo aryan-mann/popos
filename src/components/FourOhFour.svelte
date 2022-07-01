8fad6c2108f45392efaa31fc1a92e357d750fb79
Govind Brahmanyapura <brahmany@grinnell.edu>
2022-07-01 18:41:18 -0500

    better 404 handling

---
+++ src/components/FourOhFour.svelte
@@ -0,0 +1,5 @@
+<div class="h-64 flex flex-col justify-center items-center">
+	<div class="font-thin text-6xl">404</div>
+	<div class="font-thin text-4xl">Are you lost?</div>
+	<a class="font-thin text-2xl underline" href="/">Go home</a>
+</div>
