f060fb15d316e5a2ee15efbf94a107cb59cf2072
Govind Brahmanyapura <brahmany@grinnell.edu>
2022-07-02 14:45:22 -0500

    add feather-icons and icons component

--- src/routes/cities/[slug].svelte
+++ src/routes/cities/[slug].svelte
@@ -3,6 +3,7 @@
 	import { location } from '../../state';
 	import Popo from './components/Popo.svelte';
 	import CityMap from './components/CityMap.svelte';
+	import Icon from '../../components/Icon.svelte';
 
 	/** @type {import('./__types/[slug]').Load} */
 	export async function load({ params, fetch, session, stuff }) {
@@ -73,36 +74,27 @@
 			resizeMap();
 		}}
 	>
-		<div class="flex items-end text-lg">
+		<div class="flex gap-2 text-lg">
 			{#if !showMap}
 				Map View
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					class="mx-2"
-					><path
-						d="M12 0c-3.148 0-6 2.553-6 5.702 0 4.682 4.783 5.177 6 12.298 1.217-7.121 6-7.616 6-12.298 0-3.149-2.851-5.702-6-5.702zm0 8c-1.105 0-2-.895-2-2s.895-2 2-2 2 .895 2 2-.895 2-2 2zm12 16l-6.707-2.427-5.293 2.427-5.581-2.427-6.419 2.427 4-9 3.96-1.584c.38.516.741 1.08 1.061 1.729l-3.523 1.41-1.725 3.88 2.672-1.01 1.506-2.687-.635 3.044 4.189 1.789.495-2.021.465 2.024 4.15-1.89-.618-3.033 1.572 2.896 2.732.989-1.739-3.978-3.581-1.415c.319-.65.681-1.215 1.062-1.731l4.021 1.588 3.936 9z"
-					/></svg
-				>
+				<Icon
+					direction={'n'}
+					stroke="black"
+					strokeWidth={1}
+					name={'map'}
+					height="1.5em"
+					width="1.5em"
+				/>
 			{:else}
-				List View
-				<svg
-					clip-rule="evenodd"
-					fill-rule="evenodd"
-					stroke-linejoin="round"
-					stroke-miterlimit="2"
-					viewBox="0 0 24 24"
-					height="24"
-					width="24"
-					class="mx-2"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						d="m22 16.75c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm0-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75z"
-						fill-rule="nonzero"
-					/></svg
-				>
+				Grid View
+				<Icon
+					direction={'n'}
+					stroke="black"
+					strokeWidth={1}
+					name={'grid'}
+					height="1.5em"
+					width="1.5em"
+				/>
 			{/if}
 		</div>
 	</button>
