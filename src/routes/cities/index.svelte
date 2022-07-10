a778def853313df714c37e24ff076a2df179d30d
Govind Brahmanyapura <brahmany@grinnell.edu>
2022-07-09 19:13:32 -0500

    improve animation on city link hover

--- src/routes/cities/index.svelte
+++ src/routes/cities/index.svelte
@@ -1,47 +1,52 @@
 <script context="module" lang="ts">
-  import type { CityT } from "src/types";
-  import { formatTitle } from "../../utils";
+	import type { CityT } from 'src/types';
+	import { formatTitle } from '../../utils';
 
-  /** @type {import('./__types/[slug]').Load} */
-  export async function load({fetch}) {
-    const response = await fetch(`/api/cities.json`);
-    const cities: CityT[] = await response.json();
-    
-    return {
-      status: response.status,
-      props: {
-        cities,
-      }
-    };
-  }
+	/** @type {import('./__types/[slug]').Load} */
+	export async function load({ fetch }) {
+		const response = await fetch(`/api/cities.json`);
+		const cities: CityT[] = await response.json();
+
+		return {
+			status: response.status,
+			props: {
+				cities
+			}
+		};
+	}
 </script>
 
 <script lang="ts">
-  import Container from "../../components/Container.svelte";
-  export let cities: CityT[] = [];
+	import Container from '../../components/Container.svelte';
+	export let cities: CityT[] = [];
 </script>
 
 <svelte:head>
-  <title>{formatTitle('Cities')}</title>
+	<title>{formatTitle('Cities')}</title>
 </svelte:head>
 
 <div class="max-w-screen-sm m-auto">
-  <h1 class="pb-8 text-5xl text-center">Places with some places</h1>
-  <div class="flex flex-col space-y-4">
-    {#each cities as city, i}
-      <Container 
-        containerProps={{
-          class: "relative h-64 rounded-lg shadow-xl flex" 
-        }}
-        bgImageUrl={`/images/${city.slug}/${city.imageName}`}
-        bgColor="rgba(255,255,255,0.40)"
-      >
-        <div class="flex flex-grow px-8 py-8 rounded">
-          <a class="self-center px-2 py-2 text-3xl transition-colors duration-200 rounded bg-secondary-200 hover:bg-secondary-400" href={`/cities/${city.slug}`}>
-            {city.displayName}
-          </a>
-        </div>
-      </Container>
-    {/each}
-  </div>
+	<h1 class="pb-8 text-5xl text-center">Places with some places</h1>
+	<div class="flex flex-col space-y-4">
+		{#each cities as city, i}
+			<Container
+				containerProps={{
+					class: 'relative h-64 rounded-lg shadow-xl flex transform hover:scale-[1.01] duration-300'
+				}}
+				bgImageUrl={`/images/${city.slug}/${city.imageName}`}
+				bgColor="rgba(255,255,255, 0.6)"
+			>
+				<a
+					class="flex flex-grow px-8 py-8 rounded duration-300"
+					href={`/cities/${city.slug}`}
+				>
+					<div
+						class="self-end px-2 py-2 transition-colors text-5xl text-stone-900 font-thin duration-200 rounded"
+					>
+						{city.displayName}
+					</div>
+				</a>
+			</Container>
+		{/each}
+	</div>
 </div>
